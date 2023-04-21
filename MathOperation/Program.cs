13313455a68c9ca2f3207a005ff61d3084e8e696
sagisijandy <98680878+sagisijandy@users.noreply.github.com>
2023-04-20 21:31:35 -0700

    Add project files.

---
+++ MathOperation/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAthOperationAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int product = 50 * 50;
+            Console.WriteLine("Fifty times Fifty=" + product.ToString());
+            Console.ReadLine();
+
+            int total = 5 + 25;
+            Console.WriteLine("Five plus Twenty Five =" + total.ToString());
+            Console.ReadLine();
+
+            double qoutient = 125.0 / 12.5;
+            Console.WriteLine("One hundred twenty five divide by twelve point five=" + qoutient.ToString());
+            Console.ReadLine();
+
+            bool trueOrFalse = 60 > 50;
+            Console.Write("Is Sixty greater than Fifty:" + trueOrFalse.ToString());
+            Console.ReadLine();
+
+            int remainder = 50 % 7;
+            Console.Write("The raminder of Fifty divide by 7 is:" + remainder.ToString());
+            Console.ReadLine();
+        }
+    }
+}
